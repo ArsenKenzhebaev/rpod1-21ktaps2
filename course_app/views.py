1b8fa3e72ec657e4611fc71eaeaca4bf0e8e9a26
ArsenKenzhebaev <arsenal.barselona2005@gmail.com>
2024-05-30 15:30:26 +0500

    first project

---
+++ course_app/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import NewUserForm
+from .models import Course, Lesson 
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def home_page(request):
+    return render(request, "./course/home.html")
+    
+def courses_page(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
+    return render(request, "./course/courses.html", context)
+
+def lessons_page(request, slug=None):
+    course = get_object_or_404(Course, slug=slug)
+    lessons = Lesson.objects.all()  
+    if slug:
+        course = get_object_or_404(Course, slug=slug)
+        lessons = lessons.filter(course=course)
+
+    context = {
+        'course': course,
+        'lessons': lessons
+    }
+    return render(request, "./course/lessons.html", context)
+
+def lessons_detail_page(request, slug, pk):
+    course = get_object_or_404(Course, slug=slug)
+    lesson = get_object_or_404(Lesson, pk=pk)
+    context = {
+        'course': course,
+        'lesson': lesson
+    }
+    return render(request, "./course/lessons-delail.html", context)
+
+def sign_up_page(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_page')
+    else:
+        form = NewUserForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "./user/sign-up.html", context)
+
+def login_page(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('courses_page')    
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "./user/login.html", context)
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('home_page')
